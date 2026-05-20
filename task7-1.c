be4eb327849360b46d6d4fa333fbf476675b6c3f
ittk1110 <161736063+ittk1110@users.noreply.github.com>
2026-05-21 01:07:02 +0900

    update

---
+++ task7-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char filename[256]; 
+    FILE *file;        
+
+
+    scanf("%255s", filename);  
+
+    file = fopen(filename, "r");
+
+    if (file) {
+
+        printf("そのファイルは存在します。\n");
+        fclose(file);  
+    } else {
+        printf("そのファイルは存在しません。\n");
+    }
+
+    return 0;
+}
